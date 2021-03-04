e585c003960f4f13207436c711aa32061477c030
jpadillaa <jpadillaa@gmail.com>
2021-03-03 21:56:54 -0500

    Ejemplo 3 - Zero values. Se imprime el valor por defecto de un conjunto de variales comunmente utilizadas en diferentes lenguajes. Nota: Go no tiene Null

---
+++ ejemplo3.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Valores por defecto (Zero values)
+	var variable1 int
+	var variable2 float64
+	var variable3 string
+	var variable4 bool
+
+	fmt.Println("variable1: ", variable1)
+	fmt.Println("variable2: ", variable2)
+	fmt.Println("variable3: ", variable3)
+	fmt.Println("variable4: ", variable4)
+}
